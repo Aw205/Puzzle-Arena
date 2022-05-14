b4ecb086d3e6deb475926ad8b785118f8992a88c
Andrew Wang <antwang@ucsc.edu>
2022-05-14 12:39:28 -0700

    fixed matching to allow for combos

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,4 @@
 
-
 class Board extends Phaser.GameObjects.Container{
 
     constructor(scene,x,y){
@@ -9,10 +8,13 @@ class Board extends Phaser.GameObjects.Container{
 
         this.BOARD_HEIGHT = 5;
         this.BOARD_WIDTH = 6;
-        this.currentOrb = null;
+        this.cursorOrb = null;
         this.orbArray = null;
+        this.skyfallArray = null;
+
         this.startX=0;
         this.startY=0;
+        this.comboList = [];
 
         this.on("pointerdown",this.onPointerDown);
         this.on("pointerup",this.onPointerUp);
@@ -22,7 +24,6 @@ class Board extends Phaser.GameObjects.Container{
         this.setPosition(200,300);
         this.setSize(this.BOARD_WIDTH*Orb.WIDTH,this.BOARD_HEIGHT*Orb.HEIGHT);
      
-
         this.generateBoard();
        
         this.scene.physics.add.existing(this);
@@ -37,10 +38,10 @@ class Board extends Phaser.GameObjects.Container{
         let row = parseInt((pointer.y-220)/32);
         let col = parseInt((pointer.x-100)/32);
         
-        this.currentOrb = this.orbArray[row][col];
+        this.cursorOrb = this.orbArray[row][col];
 
-        this.startX= this.currentOrb.x;
-        this.startY= this.currentOrb.y;
+        this.startX= this.cursorOrb.x;
+        this.startY= this.cursorOrb.y;
 
     }
 
@@ -53,139 +54,172 @@ class Board extends Phaser.GameObjects.Container{
         let deltaY = 300-dragY;
 
         if(this.startX-deltaX >= -this.width/2 && (this.startX-deltaX <= -this.width/2 + 5*32)){
-            this.currentOrb.setX(this.startX-deltaX);
+            this.cursorOrb.setX(this.startX-deltaX);
         }
 
         if(this.startY-deltaY >= -this.height/2 && (this.startY-deltaY <= -this.height/2 + 4*32)){
-            this.currentOrb.setY(this.startY-deltaY);
+            this.cursorOrb.setY(this.startY-deltaY);
         }
 
         let current = this.orbArray[row][col]; 
-        if(current != this.currentOrb){
+        if(current != this.cursorOrb){
 
-            [this.currentOrb.startingX,current.startingX] = [current.startingX,this.currentOrb.startingX];
-            [this.currentOrb.startingY,current.startingY] = [current.startingY,this.currentOrb.startingY];
-
-            current.x=current.startingX;
-            current.y=current.startingY;
-           
+            [this.cursorOrb.startPos, current.startPos] = [current.startPos,this.cursorOrb.startPos]; 
+            current.setPosition(current.startPos.x,current.startPos.y); 
+        
             //update row and column
 
-            [this.currentOrb.row,current.row] = [current.row,this.currentOrb.row];
-            [this.currentOrb.col,current.col] = [current.col,this.currentOrb.col];
+            [this.cursorOrb.row,current.row] = [current.row,this.cursorOrb.row];
+            [this.cursorOrb.col,current.col] = [current.col,this.cursorOrb.col];
 
             [this.orbArray[row][col], this.orbArray[current.row][current.col]] = [this.orbArray[current.row][current.col],this.orbArray[row][col]];
          
         }
-
-       
-       
     }
 
     onDragEnd(pointer,dragX,dragY){
 
-        this.currentOrb.setToStartPosition();
+        this.cursorOrb.setToStartPosition();
     }
 
     onPointerUp(pointer,localX,localY,event){
 
-        this.currentOrb.setToStartPosition();
+        this.cursorOrb.setToStartPosition();
         this.solveBoard();
-
-        this.skyfall();
+        //this.skyfall();
     }
-
-
 
     generateBoard(){
 
        let rand = Phaser.Math.Between(0,4);
-
        this.orbArray = new Array(this.BOARD_HEIGHT);
+       this.skyfallArray = new Array(this.BOARD_HEIGHT);
         for(var i =0; i<this.BOARD_HEIGHT;i++){
             this.orbArray[i]= new Array(this.BOARD_WIDTH);
+            this.skyfallArray[i]= new Array(this.BOARD_WIDTH);
         }
+
         for(var row = 0; row < this.BOARD_HEIGHT;row++){
             for(var col =0; col < this.BOARD_WIDTH;col++){
                 rand = Phaser.Math.Between(0,4);
-                this.orbArray[row][col] = new Orb(this.scene,-this.width/2+col*32,-this.height/2+row*32,row,col,this.orbImages[rand]);
-                this.orbArray[row][col].startingX=-this.width/2+col*32;
-                this.orbArray[row][col].startingY=-this.height/2+row*32;
+                let x = -this.width/2+col*Orb.WIDTH;
+                let y = -this.height/2+row*Orb.HEIGHT;
+
+                this.orbArray[row][col] = new Orb(this.scene,x,y,row,col,this.orbImages[rand]);
                 this.orbArray[row][col].type = rand;
+
+                this.skyfallArray[row][col] = new Orb(this.scene,x,y-5*Orb.HEIGHT,row,col,this.orbImages[rand]);
+                this.skyfallArray[row][col].type = rand;
+                this.skyfallArray[row][col].setVisible(false);
+
                 this.add(this.orbArray[row][col]);
+
+                this.add(this.skyfallArray[row][col]);
                
             }
         }
     }
 
-
     solveBoard(){
 
-        for(var i = 0; i < this.BOARD_HEIGHT;i++){
-            for(var  j = 0; j < this.BOARD_WIDTH;j++){
-                if(!this.orbArray[i][j].isMatched){
-                    this.findMatches(this.orbArray[i][j],i,j);
-                }
-            }
-        }
+        this.matchBoard();
+        console.log("Number combos: " + this.comboList.length);
 
-        for(var i = 0; i < this.BOARD_HEIGHT;i++){
+        /* for(var i = 0; i < this.BOARD_HEIGHT;i++){
             for(var  j = 0; j < this.BOARD_WIDTH;j++){
                 if(this.orbArray[i][j].isMatched){
                     this.orbArray[i][j].destroy();
                     this.orbArray[i][j]=null;
                 }
             }
-        }
+        } */
 
     }
 
-    findMatches(current,row,col){
+    matchBoard(){
 
-        var columns = [];
-        var rows = [];
-        var pos = [2,1,-1,-2];
+        let connectedOrbs = [];
 
-        var colCounter =0;
-        var rowCounter =0;
-
-        for(var n = 0; n < 3; n++){
-            for(var i = n ; i< n +2; i++){
-                if(row + pos[i] < this.BOARD_HEIGHT && row + pos[i] > -1 ){
-                    if(this.orbArray[row + pos[i]][col].type == current.type){
-                        colCounter++;
-                        columns.push(this.orbArray[row + pos[i]][col]);
-                    }
+        for(var row = 0; row < this.BOARD_HEIGHT;row++){
+            for(var  col = 0; col < this.BOARD_WIDTH;col++){
+                let set = new Set();
+                let current = this.orbArray[row][col];          
+                if(!current.isVisited){
+                    current.isVisited=true;
+                    this.floodfill(row,col,current.type,set);
                 }
-
-                if(col + pos[i] < this.BOARD_WIDTH && col + pos[i] > -1 ){
-                    if(this.orbArray[row][col + pos[i]].type == current.type){
-                        rowCounter++;
-                        rows.push(this.orbArray[row][col + pos[i]]);
-                    }
+                if(set.size>2){
+                    connectedOrbs.push(set);
                 }
             }
-
-            if(colCounter == 2){
-                current.isMatched=true;
-                for(var orb of columns){
-                    orb.isMatched=true;
-                }
-            }
-            if(rowCounter ==2){
-                current.isMatched =true;
-                for(var orb of rows){
-                    orb.isMatched=true;
-                }
-            }
-
-            columns = [];
-            rows = [];
-            colCounter=0;
-            rowCounter=0;
-
         }
 
+        for(let set of connectedOrbs){
+           /*  arr.sort(this.compareRows); // checking horizontal matches
+            let rowList = []; 
+            for(var i =0 ; i< arr.length-1; i++){
+                if(arr[i].row == arr[i+1].row){
+                    rowList.push(arr[i]);
+                }
+                else{
+                    rowList = [];
+                }
+            }
+            arr.sort(this.compareCols);
+            let colList = [];
+            for(var i =0 ; i< arr.length -1; i++){
+                if(!rowList.includes(arr[i]) && arr[i].col==arr[i+1].col){
+                    colList.push(arr[i]);
+                }
+                else{
+                    colList = [];
+                }
+            } */
+                this.comboList.push(set);
+        }
+    }
+
+    compareRows(orb1,orb2){
+        return orb1.row - orb2.row;
+    }
+    compareCols(orb1,orb2){
+        return orb1.col - orb2.col;
+    }
+
+    floodfill(row,col,type,matchedList){
+
+        let numHorizontal =0;
+        let numVertical = 0;
+        let adj_arr =[];
+
+        for(var i = 0;i<4;i++){
+            let x=(i-1)%2;    // -1 0 1 0
+			let y= (3-i-1)%2; // 0 1 0 -1
+            if(this.isInBounds(row+x,col+y)) {
+                let adj = this.orbArray[row + x][col + y];
+                if(adj.type == type){
+                    (x==0) ? numHorizontal++ : numVertical++;
+                    if(!adj.isVisited){
+                        adj_arr.push(adj);
+                    }
+                }
+              }
+        }
+
+        for(let orb of adj_arr){
+            orb.isVisited=true;
+            this.floodfill(orb.row,orb.col,type,matchedList);
+        }
+        if(numHorizontal==2){
+            matchedList.add(this.orbArray[row][col]);
+            matchedList.add(this.orbArray[row][col-1]);
+            matchedList.add(this.orbArray[row][col+1]);
+        }
+        if(numVertical==2){
+            matchedList.add(this.orbArray[row][col]);
+            matchedList.add(this.orbArray[row-1][col]);
+            matchedList.add(this.orbArray[row+1][col]);
+        }
     }
 
     skyfall(){
@@ -203,11 +237,19 @@ class Board extends Phaser.GameObjects.Container{
                    dropDist++;
                 }
                 else{
-                   current.targetPos.set(current.x,-this.height/2+(row+dropDist)*32);
-                   this.scene.physics.moveToObject(current,current.targetPos,60);
+                   current.targetPos.set(current.x,-this.height/2+(row+dropDist)*Orb.HEIGHT);
+                   this.scene.physics.moveToObject(current,current.targetPos,60,500);
                    [this.orbArray[row][col],this.orbArray[row+dropDist][col]] =  [this.orbArray[row+dropDist][col],this.orbArray[row][col]];
                 }
             }
+             // need to skyfall new orbs 
+             for(var r = this.BOARD_HEIGHT-1;r > this.BOARD_HEIGHT-dropDist-1 ;r--){
+                let current = this.skyfallArray[r][col];
+                current.setVisible(true);
+                current.targetPos.set(current.x,-this.height/2+(r-this.BOARD_HEIGHT+dropDist)*Orb.HEIGHT);
+                this.scene.physics.moveToObject(current,current.targetPos,60,500);
+                
+            } 
             dropDist =0;
         }
 
--- src/Orb.js
+++ src/Orb.js
@@ -4,34 +4,32 @@ class Orb extends Phaser.Physics.Arcade.Sprite{
     static HEIGHT = 32;
     static WIDTH = 32;
     
-
-
     constructor(scene,x,y,row,col,texture){
         super(scene,x,y,texture);
 
         this.setOrigin(0,0);
-
         this.setScale(Orb.WIDTH/this.width,Orb.HEIGHT/this.height);
-
         this.type=1;
         this.row = row;
-        this.col=col;
-        this.startingX=0;
-        this.startingY=0;
+        this.col= col;  
         this.isMatched = false;
-
+        this.isVisited = false;
         this.targetPos = new Phaser.Math.Vector2(0,0);
+        this.startPos = new Phaser.Math.Vector2(x,y);
+        this.horizontalAdjacents=0;
+        this.verticalAdjacents = 0;
+
 
         this.scene.physics.add.existing(this);
-        this.setCollideWorldBounds(true,0,0);
         this.scene.add.existing(this);
+
     }
 
     update(){
 
         let dist = Phaser.Math.Distance.Between(this.x, this.y, this.targetPos.x, this.targetPos.y);
         if(this.body.speed>0){
-            if(dist<5){
+            if(dist<2){
                 this.body.reset(this.targetPos.x,this.targetPos.y);
             }
         }
@@ -39,8 +37,7 @@ class Orb extends Phaser.Physics.Arcade.Sprite{
     }
 
     setToStartPosition(){
-        this.setPosition(this.startingX,this.startingY);
-
+        this.setPosition(this.startPos.x,this.startPos.y); 
     }
 
 }
